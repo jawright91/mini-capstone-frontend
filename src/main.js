15f116eca4dff98de2e4e6d6a73693b586cb2dfb
Joshua Wright <jawright91@gmail.com>
2021-06-15 20:43:34 -0500

    configure netlify redirects

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import axios from "axios";
 import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,104 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="row">
+      <div class="col-sm-4" v-for="product in products" v-bind:key="product.id">
+        <div class="card">
+          <div class="carousel-inner">
+            <div class="carousel-item active" v-for="image in product.images" v-bind:key="image.id">
+              <img class="d-block w-100" v-bind:src="image.url" v-bind:alt="product.title" />
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <div class="card-body">
+          <h2 class="card-title">{{ product.name }}</h2>
+          <p class="card-text">$ {{ product.price }}</p>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-toggle="modal"
+            data-target="#active-product"
+            v-on:click="setActiveProduct(product)"
+          >
+            More info
+          </button>
+        </div>
+      </div>
+
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="active-product"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">{{ activeProduct.name }}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
+  <!-- <div v-for="recipe in recipes" v-bind:key="recipe.id">
+      <h2>{{ recipe.title }}</h2>
+      <p>by {{ recipe.chef }}</p>
+      <a v-bind:href="`/recipes/${recipe.id}`">More info</a>
+    </div> -->
+  <!-- </div> -->
+
+  <!-- <div v-for="product in products" v-bind:key="product.id">
+      <h1>{{ product.name }}</h1>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+ 
+    </div> -->
+  <!-- </div> -->
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      products: ["hello"],
+      activeProduct: ["If you can see this, something is borked"],
+    };
+  },
+  created: function () {
+    this.loadProducts();
+  },
+  methods: {
+    loadProducts: function () {
+      axios.get("/products/").then((response) => {
+        console.log("poop", response.data);
+        this.products = response.data;
+      });
+    },
+    setActiveProduct: function (product) {
+      this.activeProduct = product;
+      console.log(this.activeProduct);
+    },
   },
 };
 </script>
